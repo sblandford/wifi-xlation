20fdb2a787bcc78e9d2ff347d87d7f625c679622
sblandford <simon@onepointltd.com>
2022-11-03 10:02:11 +0000

    Nginx reverse proxy to correct port

--- src/bin/xlation.sh
+++ src/bin/xlation.sh
@@ -247,7 +247,7 @@ if [[ "${HTTPS_ENABLE,,}" =~ true ]]; then
         }
 
         location ~ ^/janus($|/) {
-            proxy_pass http://127.0.0.1:9088;
+            proxy_pass http://127.0.0.1:8088;
             proxy_set_header Host \$host;
         }
         location ~ ^/admin($|/) {
