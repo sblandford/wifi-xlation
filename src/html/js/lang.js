78f39c6ec5197e42760e3d6fabde0255329a786f
sblandford <simon@onepointltd.com>
2022-11-04 14:49:42 +0000

    Trailing whitespace cleanup

--- src/html/js/lang.js
+++ src/html/js/lang.js
@@ -138,7 +138,7 @@ const LANG = {
         "show": "Показывать",
         "set password": "Установка пароля",
         "unauthorised": "Неавторизованный"
-    }    
+    }
 }
 
 /*["af", "sq", "ar-SA", "ar-IQ", "ar-EG", "ar-LY", "ar-DZ", "ar-MA", "ar-TN", "ar-OM",
@@ -146,10 +146,10 @@ const LANG = {
  "be", "ca", "zh-TW", "zh-CN", "zh-HK", "zh-SG", "hr", "cs", "da", "nl", "nl-BE", "en",
  "en-US", "en-EG", "en-AU", "en-GB", "en-CA", "en-NZ", "en-IE", "en-ZA", "en-JM",
  "en-BZ", "en-TT", "et", "fo", "fa", "fi", "fr", "fr-BE", "fr-CA", "fr-CH", "fr-LU",
- "gd", "gd-IE", "de", "de-CH", "de-AT", "de-LU", "de-LI", "el", "he", "hi", "hu", 
+ "gd", "gd-IE", "de", "de-CH", "de-AT", "de-LU", "de-LI", "el", "he", "hi", "hu",
  "is", "id", "it", "it-CH", "ja", "ko", "lv", "lt", "mk", "mt", "no", "pl",
  "pt-BR", "pt", "rm", "ro", "ro-MO", "ru", "ru-MI", "sz", "sr", "sk", "sl", "sb",
- "es", "es-AR", "es-GT", "es-CR", "es-PA", "es-DO", "es-MX", "es-VE", "es-CO", 
- "es-PE", "es-EC", "es-CL", "es-UY", "es-PY", "es-BO", "es-SV", "es-HN", "es-NI", 
+ "es", "es-AR", "es-GT", "es-CR", "es-PA", "es-DO", "es-MX", "es-VE", "es-CO",
+ "es-PE", "es-EC", "es-CL", "es-UY", "es-PY", "es-BO", "es-SV", "es-HN", "es-NI",
  "es-PR", "sx", "sv", "sv-FI", "th", "ts", "tn", "tr", "uk", "ur", "ve", "vi", "xh",
  "ji", "zu"];*/
--- src/html/js/player.js
+++ src/html/js/player.js
@@ -292,7 +292,7 @@ function classOut (id, className) {
     const classList = document.getElementById(id).classList;
     if (classList.contains(className)) {
         classList.remove(className);
-    }    
+    }
 }
 
 function classSet (id, className, state) {
@@ -305,10 +305,10 @@ function classSet (id, className, state) {
 
 
 function mobileAndTabletcheck () {
-    
+
     if (gMobileAndTabletState == null) {
         gMobileAndTabletState = false;
-        
+
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0,4))) gMobileAndTabletState = true;})(navigator.userAgent||navigator.vendor||window.opera);
     }
     return gMobileAndTabletState;
@@ -318,15 +318,15 @@ function showQr() {
     let boxWidth, boxHeight;
     const boxDiv = document.getElementById("qrBox");
     const boxObj = document.getElementById("qrObj");
-   
+
     //Needs two iterations to settle on correct size
     for (let i=0; i < 2; i++) {
         boxWidth = boxDiv.offsetWidth;
         boxHeight = boxDiv.offsetHeight;
-       
+
         boxObj.width = document.documentElement.clientWidth;
         boxObj.height = document.documentElement.clientHeight * 0.6;
-       
+
         if (boxWidth < 400) {
             const scale = boxWidth / 400;
             boxObj.style.transform = "scale(" + scale + ")";
@@ -362,7 +362,7 @@ function pollStatus () {
                     alert("Got no response to our query for available RX streams");
             } else {
                 const list = result.list;
-                Janus.debug("Got list of streams");                
+                Janus.debug("Got list of streams");
                 gSendMixerHandle.send({"message": body, success: function(result) {
                     let listTx = false;
                     if(result === null || result === undefined) {
@@ -407,7 +407,7 @@ function pollStatus () {
                                 initaliseLocalStorageIfRequired(gStatus[0].name);
                             }
                         updateDisplay();
-                    }                    
+                    }
                 }});
             }
         }});
@@ -428,7 +428,7 @@ function channelNumberLookup (name) {
             if (name.toUpperCase() == gStatus[channel].name.toUpperCase()) {
                 return channel;
             }
-        }        
+        }
     }
     return -1;
 }
@@ -439,7 +439,7 @@ function updateDisplay() {
     for (let channel = 0; channel < gStatus.length; channel++) {
         let status = false, freeTx = true, validTx = false;
         let name = (parseInt(channel) + 1).toString();
-        
+
         if (gStatus[channel].hasOwnProperty('valid')) {
             status = gStatus[channel].valid;
         }
@@ -462,7 +462,7 @@ function updateDisplay() {
         } else {
             listHtml += "<a href=\"#\" class=\"disabled chNameNormal\">" + name + "</a>\n";
         }
-        
+
         if (validTx) {
             listHtmlTx +="<a href=\"#\"" +
             " class=\"" + ((freeTx)?"chNameNormal":"chNameBusy") + "\"" +
@@ -486,7 +486,7 @@ function updateDisplay() {
                 startStopButtonId.innerText = LANG[gLang][(gPlayIntention)?'stop':'start'];
                 startStopButtonId.disabled = !gPlayIntention;
             }
-            
+
         }
         if (name == localStorage.channelTx) {
             const chNameId = document.getElementById('chNameTx');
@@ -500,19 +500,19 @@ function updateDisplay() {
             if (validTx) {
                 classSet('chNameTx', 'chNameBusy', !freeTx);
                 startMuteButtonId.innerText = startMuteButtonText;
-                startMuteButtonId.disabled = false;             
+                startMuteButtonId.disabled = false;
             } else {
                 classOut('chNameTx', 'chNameBusy');
                 startMuteButtonId.innerText = LANG[gLang][(gSendIntention)?'mute':'broadcast'];
                 startMuteButtonId.disabled = !gSendIntention;
             }
         }
-    }    
+    }
     document.getElementById('chSelectList').innerHTML = listHtml;
     document.getElementById('chSelectListTx').innerHTML = listHtmlTx;
     document.getElementById('chSelectBtn').innerText = LANG[gLang].select;
     document.getElementById('chSelectBtnTx').innerText = LANG[gLangTx].select;
-    document.getElementById('chSelectBtnTx').disabled = gSending;    
+    document.getElementById('chSelectBtnTx').disabled = gSending;
     document.getElementById('stopButtonTx').innerText = LANG[gLangTx].stop;
     document.getElementById('passFailBox').innerHTML = LANG[gLangTx].unauthorised;
     document.getElementById('passOKButton').innerText = LANG[gLangTx].ok;
@@ -537,7 +537,7 @@ function updateDisplay() {
     }
     document.getElementById('stopButtonTx').style.visibility = (gSendIntention)?"visible":"hidden";
     classSet('micImg', 'iconDisabled', gSendIntention);
-    classSet('keyImg', 'iconDisabled', gSendIntention);    
+    classSet('keyImg', 'iconDisabled', gSendIntention);
 }
 
 //Drop down menu related
@@ -566,7 +566,7 @@ window.onclick = function(event) {
             openDropdown.classList.remove('show');
             }
         }
-    }  
+    }
     //Hide QR code if showing
     if (!event.target.matches('.qrBtn')) {
         classOut('qrBox', 'qrShow');
@@ -654,12 +654,12 @@ function onclickStartTx() {
 function ontouchendStart() {
     if (mobileAndTabletcheck() && !document.getElementById('startStopButton').disabled) {
         startPlay();
-    }    
+    }
 }
 function ontouchendStartTx() {
     if (mobileAndTabletcheck() && !document.getElementById('startStopButtonTx').disabled) {
         startSend();
-    }    
+    }
 }
 function onclickChannel(channel) {
     if (!mobileAndTabletcheck()) {
@@ -703,7 +703,7 @@ function ontouchendChannelTx(channel) {
         if (!gPasswordsTx[localStorage.channelTx]) {
             gPasswordsTx[localStorage.channelTx] = gDefaultPassword;
             localStorage.passwordsTx = JSON.stringify(gPasswordsTx);
-        }      
+        }
         updateDisplay();
         if (gSending) {
             startSend();
@@ -834,7 +834,7 @@ function keyDialog() {
     const passDiv = document.getElementById('passBox');
     const passText = LANG[gLang]['set password'] + " : " + document.getElementById('chNameTx').innerHTML;
     // Only allow password dialog to appear if not broadcasting
-    if (!gSendIntention || passDiv.classList.contains('passShow')) {    
+    if (!gSendIntention || passDiv.classList.contains('passShow')) {
         passwordKeyUp();
         classIn('passBox', 'passShow');
         document.getElementById('passBoxText').innerHTML = passText;
@@ -844,7 +844,7 @@ function keyDialog() {
 
 function passClose() {
     classOut('passBox', 'passShow');
-    classOut('panelTx', 'blocker'); 
+    classOut('panelTx', 'blocker');
 }
 
 function passCancel() {
@@ -877,7 +877,7 @@ function passShow () {
         pwBox.type = 'text';
     } else {
         pwBox.type = 'password';
-    }    
+    }
 }
 
 function passOKClick() {
